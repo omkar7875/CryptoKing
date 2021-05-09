a2a2b1c81bc1897baf5e7bfdd6cbea109d94447d
Omkar <omkarsakpal2001@gmail.com>
2021-05-09 21:47:37 +0530

    Android project

---
+++ app/src/main/java/com/example/cryptoking/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.cryptoking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button enc,dec,abt;
+    ViewFlipper vf;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        enc = findViewById(R.id.encd);
+        dec = findViewById(R.id.decd);
+        enc.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                    Intent temp = new Intent(MainActivity.this,Encoder.class);
+                    startActivity(temp);
+                }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent temp =  new Intent(MainActivity.this,Decoder.class);
+                startActivity(temp);
+            }
+        });
+
+
+        vf=findViewById(R.id.vf);
+        int images[] = {R.drawable.bellaso_cipher,R.drawable.caesar_cipher_encryption,R.drawable.dorabella_cipher};
+
+        for(int i = 0;i<images.length;i++){
+            flipper(images[i]);
+        }
+    }
+    public void flipper(int images){
+        ImageView test=new ImageView(this);
+        test.setBackgroundResource(images);
+        vf.addView(test);
+        vf.setFlipInterval(3000);
+        vf.setAutoStart(true);
+        vf.setInAnimation(this,android.R.anim.slide_in_left);
+        vf.setOutAnimation(this,android.R.anim.slide_out_right);
+    }
+}
+
